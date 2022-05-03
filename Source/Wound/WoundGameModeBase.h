2a2c653ac50367daea1893040f5d043b6396f532
BloodRedTape <gefestroyale@gmail.com>
2022-05-03 23:39:55 +0300

    Initial

---
+++ Source/Wound/WoundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WoundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WOUND_API AWoundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
